49b34f48c2b20214b4f7a4ae64bcc9f5ad5122f4
Nicholas Laughter <nicholaslaughter@LaughterBook-Pro.local>
2017-01-16 15:04:38 -0700

    UI Elements for office

---
+++ Chiropractic/SignatureCaptureView.swift
@@ -0,0 +1,21 @@
+//
+//  SignatureCaptureView.swift
+//  Chiropractic
+//
+//  Created by Nicholas Laughter on 1/16/17.
+//  Copyright © 2017 Nicholas Laughter. All rights reserved.
+//
+
+import UIKit
+
+class SignatureCaptureView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
